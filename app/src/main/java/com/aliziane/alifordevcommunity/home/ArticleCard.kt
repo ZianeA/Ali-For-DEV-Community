fc43274d949b7742e8126679b36e86d36a2c065a
ZianeA <wit_wit90@hotmail.com>
2021-09-23 10:46:42 +0100

    Refactor article card

---
+++ app/src/main/java/com/aliziane/alifordevcommunity/home/ArticleCard.kt
@@ -0,0 +1,196 @@
+package com.aliziane.alifordevcommunity.home
+
+import android.content.res.Configuration
+import android.text.format.DateUtils
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BookmarkAdd
+import androidx.compose.material.icons.outlined.ChatBubbleOutline
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.aliziane.alifordevcommunity.R
+import com.aliziane.alifordevcommunity.common.Tag
+import com.aliziane.alifordevcommunity.common.fakeArticle
+import com.aliziane.alifordevcommunity.common.toPrettyCount
+import com.aliziane.alifordevcommunity.ui.theme.AliForDEVCommunityTheme
+
+@Composable
+fun Article(modifier: Modifier = Modifier, article: Article, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.medium)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.18f),
+                shape = MaterialTheme.shapes.medium
+            )
+            .clickable(onClick = onClick)
+    ) {
+        CoverImage(article.coverImageUrl)
+
+        Column(
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 16.dp,
+                bottom = 8.dp
+            )
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                AuthorAvatar(article.author.avatarUrl)
+
+                Column(modifier = Modifier.padding(start = 8.dp)) {
+                    Text(text = article.author.name, style = MaterialTheme.typography.subtitle2)
+                    PublishDate(article.publishedAt.time)
+                }
+            }
+
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = article.title,
+                style = MaterialTheme.typography.h6
+            )
+
+            Tags(article.tags)
+
+            Metadata(modifier, article)
+        }
+    }
+}
+
+@Composable
+private fun Metadata(
+    modifier: Modifier,
+    article: Article
+) {
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = modifier.weight(1f),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.FavoriteBorder,
+                contentDescription = "Reactions"
+            )
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = article.reactionCount.toPrettyCount()
+            )
+            Icon(
+                modifier = Modifier.padding(start = 16.dp),
+                imageVector = Icons.Outlined.ChatBubbleOutline,
+                contentDescription = "Comments"
+            )
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = article.commentCount.toPrettyCount()
+            )
+        }
+        Row(
+            modifier = modifier.weight(1f),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = stringResource(R.string.read_time, article.readTimeMinutes))
+            Button(modifier = Modifier.padding(start = 8.dp), onClick = { /*TODO*/ }) {
+                Text("Save")
+            }
+        }
+    }
+}
+
+@Composable
+private fun Tags(tags: List<String>) {
+    Row {
+        for ((index, tag) in tags.withIndex()) {
+            if (index > 0) {
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+            Tag(text = tag) {/*TODO*/ }
+        }
+    }
+}
+
+@Composable
+private fun PublishDate(time: Long) {
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = DateUtils.getRelativeTimeSpanString(time).toString(),
+            style = MaterialTheme.typography.body2
+        )
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+private fun AuthorAvatar(avatarUrl: String) {
+    Image(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(shape = CircleShape)
+            .background(MaterialTheme.colors.onSurface.copy(alpha = 0.18f)),
+        painter = rememberImagePainter(avatarUrl) {
+            crossfade(true)
+            placeholder(R.drawable.ic_person)
+        },
+        contentDescription = "Author Avatar",
+        contentScale = ContentScale.Crop
+    )
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+private fun CoverImage(coverImageUrl: String?) {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            // A fix for image not loading when height is unbounded.
+            // See https://issuetracker.google.com/issues/186012457
+            .heightIn(min = 1.dp),
+        painter = rememberImagePainter(coverImageUrl) {
+            crossfade(true)
+            placeholder(R.drawable.ic_image)
+        },
+        contentDescription = "Article cover",
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun ArticleDarkPreview() {
+    AliForDEVCommunityTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Article(article = fakeArticle)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ArticleLightPreview() {
+    AliForDEVCommunityTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Article(article = fakeArticle)
+        }
+    }
+}
--- app/src/main/java/com/aliziane/alifordevcommunity/home/HomeScreen.kt
+++ app/src/main/java/com/aliziane/alifordevcommunity/home/HomeScreen.kt
@@ -1,37 +1,19 @@
 package com.aliziane.alifordevcommunity.home
 
 import android.content.res.Configuration
-import android.text.format.DateUtils
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.BookmarkAdd
-import androidx.compose.material.icons.outlined.ChatBubbleOutline
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import com.aliziane.alifordevcommunity.R
 import com.aliziane.alifordevcommunity.common.*
-import com.aliziane.alifordevcommunity.common.toPrettyCount
 import com.aliziane.alifordevcommunity.ui.theme.AliForDEVCommunityTheme
 
 @Composable
@@ -78,145 +60,4 @@ private fun TopBar(onOpenDrawer: () -> Unit) {
                 Icon(imageVector = Icons.Default.Menu, contentDescription = "Drawer")
             }
         })
-}
-
-@OptIn(ExperimentalCoilApi::class)
-@Composable
-private fun Article(modifier: Modifier = Modifier, article: Article, onClick: () -> Unit = {}) {
-    val borderColor = MaterialTheme.colors.onSurface.copy(alpha = 0.18f)
-    Column(
-        modifier = modifier
-            .clip(MaterialTheme.shapes.medium)
-            .border(
-                width = 1.dp,
-                color = borderColor,
-                shape = MaterialTheme.shapes.medium
-            )
-            .clickable(onClick = onClick)
-    ) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                // A fix for image not loading when height is unbounded.
-                // See https://issuetracker.google.com/issues/186012457
-                .heightIn(min = 1.dp),
-            painter = rememberImagePainter(article.coverImageUrl) {
-                crossfade(true)
-                placeholder(R.drawable.ic_image)
-            },
-            contentDescription = "Article cover",
-            contentScale = ContentScale.FillWidth
-        )
-
-        Column(
-            modifier = Modifier.padding(
-                start = 16.dp,
-                end = 16.dp,
-                top = 16.dp,
-                bottom = 8.dp
-            )
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(shape = CircleShape)
-                        .background(MaterialTheme.colors.onSurface.copy(alpha = 0.18f)),
-                    painter = rememberImagePainter(article.author.avatarUrl) {
-                        crossfade(true)
-                        placeholder(R.drawable.ic_person)
-                    },
-                    contentDescription = "Author Avatar",
-                    contentScale = ContentScale.Crop
-                )
-
-                Column(modifier = Modifier.padding(start = 8.dp)) {
-                    Text(text = article.author.name, style = MaterialTheme.typography.subtitle2)
-                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                        Text(
-                            text = DateUtils.getRelativeTimeSpanString(article.publishedAt.time)
-                                .toString(),
-                            style = MaterialTheme.typography.body2
-                        )
-                    }
-                }
-            }
-
-            Text(
-                modifier = Modifier.padding(vertical = 8.dp),
-                text = article.title,
-                style = MaterialTheme.typography.h6
-            )
-
-            Row {
-                for ((index, tag) in article.tags.withIndex()) {
-                    if (index > 0) {
-                        Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
-                    }
-                    Tag(text = tag) {/*TODO*/ }
-                }
-            }
-
-            Row(
-                modifier = Modifier.padding(top = 8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Row(
-                    modifier = modifier.weight(1f),
-                    horizontalArrangement = Arrangement.Start,
-                ) {
-                    Icon(
-                        imageVector = Icons.Outlined.FavoriteBorder,
-                        contentDescription = "Reactions"
-                    )
-                    Text(
-                        modifier = Modifier.padding(start = 8.dp),
-                        text = article.reactionCount.toPrettyCount()
-                    )
-                    Icon(
-                        modifier = Modifier.padding(start = 16.dp),
-                        imageVector = Icons.Outlined.ChatBubbleOutline,
-                        contentDescription = "Comments"
-                    )
-                    Text(
-                        modifier = Modifier.padding(start = 8.dp),
-                        text = article.commentCount.toPrettyCount()
-                    )
-                }
-                Row(
-                    modifier = modifier.weight(1f),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = stringResource(R.string.read_time, article.readTimeMinutes))
-                    IconButton(modifier = Modifier.padding(start = 8.dp), onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Outlined.BookmarkAdd,
-                            contentDescription = "Bookmark"
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun ArticleDarkPreview() {
-    AliForDEVCommunityTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Article(article = fakeArticle)
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ArticleLightPreview() {
-    AliForDEVCommunityTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Article(article = fakeArticle)
-        }
-    }
 }
